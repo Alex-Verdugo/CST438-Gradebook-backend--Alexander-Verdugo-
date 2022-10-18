8f3a719de50bf2bef64dfa431559484fbd2f8b65
Alex-Verdugo <73000648+Alex-Verdugo@users.noreply.github.com>
2022-10-18 10:03:54 -0700

    Create AssignmentDTO.java

---
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentDTO.java
@@ -0,0 +1,11 @@
+package com.cst438.domain;
+
+public class AssignmentDTO {
+	public int assignmentId;
+	public String assignmentName;
+	public String dueDate;
+	public String courseTitle;
+	public int courseId;
+
+}
+x
