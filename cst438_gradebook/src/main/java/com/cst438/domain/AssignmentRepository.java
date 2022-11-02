f1fa92d4014d5980dc451a5d9bab2b61732e6d6f
Alex-Verdugo <73000648+Alex-Verdugo@users.noreply.github.com>
2022-11-01 18:50:48 -0700

    Assignment 5 Created End to end test to add assignment.
    getting error saying "courseRepository" is null

--- cst438_gradebook/src/main/java/com/cst438/domain/AssignmentRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentRepository.java
@@ -7,6 +7,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface AssignmentRepository extends CrudRepository <Assignment, Integer> {
+	
+	@Query("select a from Assignment a where a.name = :name")
+	Assignment findByName(@Param("name") String name);
+
 
 	@Query("select a from Assignment a where a.needsGrading=1 and a.dueDate < current_date and a.course.instructor= :email order by a.id")
 	List<Assignment> findNeedGradingByEmail(@Param("email") String email);
--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -20,7 +20,9 @@ public class RegistrationServiceREST extends RegistrationService {
 	@Override
 	public void sendFinalGrades(int course_id , CourseDTOG courseDTO) { 
 		
-		//TODO  complete this method in homework 4
+		   System.out.println("Sending final grades "+course_id+" "+courseDTO);
+	        restTemplate.put(registration_url+"/course/"+course_id,courseDTO);
+	        System.out.println("After sending final grades");
 		
 	}
 }
